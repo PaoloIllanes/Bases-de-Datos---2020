f2d87edc74d229d71b73ae73358927ac7b85a208
PaoloIllanes <48989948+PaoloIllanes@users.noreply.github.com>
2020-03-19 17:13:37 -0600

    Add files via upload

---
+++ Querys clase 4/Ejercicios clase 4.sql
@@ -0,0 +1,10 @@
+use bookstore;
+select usuario, nombre, apellido
+from empleado inner join usuario on usuario.idempleado=empleado.idempleado;
+select email as correo
+from usuario inner join empleado on usuario.idempleado=empleado.idempleado && usuario.activo=1;
+select count(idpublicacion)
+from publicacion where autor = 'Eric G. Coronel Castillo';
+select sum(total) as 'Suma ventas'
+from empleado inner join venta
+where empleado.idempleado=venta.idempleado && empleado.nombre= 'EMILIO'
